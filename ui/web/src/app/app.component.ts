0e179aeb9159830e0ca0868e5479e39de6f9a5b8
Andrei Markin <amarkin@google.com>
2025-05-21 10:09:56 +0400

    fix: don't redirect to home page on app init
    In current implementation whenever user tries to load a particular chat, it keeps redirecting to home page.
    
    Change-Id: I8bb1f54a9df039aaacfc3a75ee2427ac3158c27a

--- ui/web/src/app/app.component.ts
+++ ui/web/src/app/app.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  Output,
-  OnInit,
-  inject,
-} from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 import { v4 as uuidv4 } from 'uuid';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -19,16 +12,13 @@ import { ChatListComponent } from './components/chat-list/chat-list.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Creative Assistant';
   newChat = true;
   private router = inject(Router);
   @Input() chatId = '';
   @Output() readonly newChatStarted = new EventEmitter<boolean>();
 
-  ngOnInit() {
-    this.startChat();
-  }
   setChat(chatId: string) {
     this.chatId = chatId;
     this.newChat = false;
